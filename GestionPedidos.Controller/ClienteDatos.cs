55554e7e68d519c491fdc4be3e4011da17026ef8
lucila790 <lucilamaier2@gmail.com>
2025-05-12 19:36:38 -0300

    Controllers

---
+++ GestionPedidos.Controller/ClienteDatos.cs
@@ -0,0 +1,61 @@
+﻿using GestionPedidos.Model;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace GestionPedidos.Controller
+{
+    public class ClienteDatos
+    {
+        public List<Cliente> ObtenerTodos()
+        {
+            List<Cliente> lista = new List<Cliente>();
+            using (SqlConnection conn = Conexion.ObtenerConexion())
+            {
+                conn.Open();
+                string consulta = "SELECT * FROM Clientes";
+                SqlCommand cmd = new SqlCommand(consulta, conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Cliente cli = new Cliente
+                    {
+                        IdCliente = (int)dr["IdCliente"],
+                        Nombre = dr["Nombre"].ToString(),
+                        Email = dr["Email"].ToString(),
+                        Telefono = dr["Telefono"].ToString()
+                    };
+                    lista.Add(cli);
+                }
+            }
+            return lista;
+        }
+
+        public List<Cliente> ObtenerPorNombre(string nombre)
+        {
+            List<Cliente> lista = new List<Cliente>();
+            using (SqlConnection conn = Conexion.ObtenerConexion())
+            {
+                conn.Open();
+                string consulta = "SELECT * FROM Clientes WHERE Nombre LIKE @nombre";
+                SqlCommand cmd = new SqlCommand(consulta, conn);
+                cmd.Parameters.AddWithValue("@nombre", "%" + nombre + "%");
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Cliente cli = new Cliente
+                    {
+                        IdCliente = (int)dr["IdCliente"],
+                        Nombre = dr["Nombre"].ToString(),
+                        Email = dr["Email"].ToString(),
+                        Telefono = dr["Telefono"].ToString()
+                    };
+                    lista.Add(cli);
+                }
+            }
+            return lista;
+        }
+    }
+}
---
+++ GestionPedidos.Controller/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace GestionPedidos.Controller
+{
+    public static class Conexion
+    {
+        // Cambiá el nombre del servidor si es necesario (localhost, .\SQLEXPRESS, etc.)
+        private static string cadenaConexion = "Server=localhost;Database=GestionPedidos;Trusted_Connection=True;";
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(cadenaConexion);
+        }
+    }
+}
